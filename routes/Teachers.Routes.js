3e80b2e83c9b608b54876b3ed3a1a3a0d1f4d056
nahuel1111 <ricardo-nahuel@hotmail.com>
2024-01-29 15:09:19 -0300

    se pone los controladores de teachers

--- routes/Teachers.Routes.js
+++ routes/Teachers.Routes.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const route = express.Router()
-const { createTeacher} = require('../controllers/Teachers.Controllers')
+const { createTeacher,GetTeachers, GetOneTeacher, DeleteTeacher, UpdateTeacher} = require('../controllers/Teachers.Controllers')
 
 
 route.post('/', createTeacher)
+route.get('/', GetTeachers)
+route.get('/:id', GetOneTeacher)
+route.put('/:id', UpdateTeacher)
+route.delete('/:id', DeleteTeacher)
 
 
 module.exports = route
