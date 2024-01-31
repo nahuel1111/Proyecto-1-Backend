77868f67492bbc071b4208d4e7c2b5f9d2778cab
nahuel1111 <ricardo-nahuel@hotmail.com>
2024-01-31 15:18:06 -0300

    se crea la parte de contraseñas encriptadas y el token

--- routes/Teachers.Routes.js
+++ routes/Teachers.Routes.js
@@ -3,11 +3,11 @@ const route = express.Router()
 const { createTeacher,GetTeachers, GetOneTeacher, DeleteTeacher, UpdateTeacher} = require('../controllers/Teachers.Controllers')
 
 
-route.post('/', createTeacher)
-route.get('/', GetTeachers)
+route.post('/', auth("admin"), createTeacher)
+route.get('/', auth("admin"), GetTeachers)
 route.get('/:id', GetOneTeacher)
-route.put('/:id', UpdateTeacher)
-route.delete('/:id', DeleteTeacher)
+route.put('/:id', auth("admin"), UpdateTeacher)
+route.delete('/:id' ,auth("admin"), DeleteTeacher)
 
 
 module.exports = route
--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 
 class Server{
     constructor() {
@@ -10,6 +11,7 @@ class Server{
     
       middlewars() {
         this.app.use(express.json())
+        this.app.use(morgan('dev'))
         this.app.use(cors())
       }
     
