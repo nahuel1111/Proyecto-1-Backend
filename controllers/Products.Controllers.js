dbc59e063943355f748893cc64a0555d5a130894
Cesar Hermida <137806786+Sexcsar@users.noreply.github.com>
2024-01-31 13:55:37 -0300

    Se traba en el backend de Productos

--- controllers/Products.Controllers.js
+++ controllers/Products.Controllers.js
@@ -3,15 +3,73 @@ const ProductsModel = require("../models/Products.Schema")
 const createProduct = async (req, res) => {
   try {
 
+    const { titulo, precio, descripcion } = req.body
 
-    const newProduct = new ProductsModel(req.body)
-
-    console.log(newProduct)
+    if (!titulo || !precio || !descripcion) {
+      res.status(400).json({ msg: 'Algun campo esta vacio' })
+      return
+    }
+    const newProduct = new ProductsModel({ ...req.body})
+    await newProduct.save()
+    res.status(201).json({ msg: 'Producto creado con exito', newProduct })
   } catch (error) {
-  console.log(error)
+    res.status(500).json({ msg: 'Falla en el server', error })
+  }   
+}
+
+const getProducts = async (req, res) => {
+  try {
+    const getAllProducts = await ProductsModel.find()
+    res.status(200).json({ msg: 'Productos encontrados', getAllProducts })
+  } catch (error) {
+    res.status(500).json({ msg: 'Falla en el server', error })
   }
 }
 
+const getOneProduct = async (req, res) => {
+  try {
+    const getProduct = await ProductsModel.findOne({ _id: req.params.id })
+    res.status(200).json({ msg: 'Producto encontrado', getProduct })
+  } catch (error) {
+    res.status(500).json({ msg: 'Falla en el server', error })
+  }
+}
+
+const updateProduct = async (req, res) => {
+  try {
+
+    const updateProduct = await ProductsModel.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    res.status(200).json({ msg: 'Producto Actualizado', updateProduct })
+
+  } catch (error) {
+    res.status(500).json({ msg: 'Falla en el server', error })
+  }
+}
+
+const deleteProduct = async (req, res) => {
+  try {
+
+    const productExist = await ProductsModel.findOne({ _id: req.params.id })
+
+    if (!productExist) {
+      res.status(400).json({ msg: 'El producto que intentas borrar no existe en la base de datos' })
+      return
+    }
+
+    await ProductsModel.findByIdAndDelete({ _id: req.params.id })
+    res.status(200).json({ msg: 'Producto eliminado' })
+  } catch (error) {
+    res.status(500).json({ msg: 'Falla en el server', error })
+  }
+}
+
+
+
 module.exports = {
-    createProduct, 
+    createProduct,
+    getProducts,
+    getOneProduct,
+    updateProduct,
+    deleteProduct,
+
   }
